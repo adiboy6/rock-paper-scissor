dfe3a96a5ba4527726799556b2b1133c384b8b73
adiboy6 <aditya.reddy.97@gmail.com>
2021-06-20 22:24:50 +0530

    added more features to save and show score

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from random import choice
+import json
 
 choices = ["Rock", "Paper", "Scissor"]
 
-
-def game():
+def game(played,p1_won,cmp_won):
+    played+=1
     print("ROCK!! PAPER!! SCISSOR!!")
     print("1. Rock")
     print("2. Paper")
@@ -14,35 +15,82 @@ def game():
 
     if player_choice == computer_choice:
         print("It's a draw")
-        return
-
-    if player_choice == 'Rock':
-        if computer_choice == 'Scissor':
-            print("You win!!")
-        else:
-            print("You lost!!")
-    elif player_choice == 'Paper':
-        if computer_choice == 'Rock':
-            print("You win!!")
-        else:
-            print("You lost")
-    elif player_choice == 'Scissor':
-        if computer_choice == 'Paper':
-            print("You win!!")
-        else:
-            print("You lost")
-
+    else:
+        if player_choice == 'Rock':
+            if computer_choice == 'Scissor':
+                print("You win!!")
+                p1_won+=1
+            else:
+                print("You lost!!")
+                cmp_won+=1
+        elif player_choice == 'Paper':
+            if computer_choice == 'Rock':
+                print("You win!!")
+                p1_won+=1
+            else:
+                print("You lost")
+                cmp_won+=1
+        elif player_choice == 'Scissor':
+            if computer_choice == 'Paper':
+                print("You win!!")
+                p1_won+=1
+            else:
+                print("You lost")
+                cmp_won+=1
+    
+    return (played,p1_won,cmp_won)
 
 if __name__ == "__main__":
     print("Welcome to Rock-Paper Scissor")
-    menu_choice=int(input("Menu:\n1.New Game\n2.Exit\n"))
-    if menu_choice == 1:
+    menu_choice=int(input(
+            "Menu:\n" +
+                "1.New Game\n" + 
+                "2.Exit\n"
+        ))
+    if menu_choice == 1:                #New Game
         player_name = input("Enter player Name:")
         print("Hello {}. Game starting...".format(player_name))
+        played,p1_won,cmp_won = 0, 0, 0
+        exit=False
         while True:
-            game()
-            play_again = input("Play again?\n(Y)es\n(N)o\n")[0].lower()
-            if play_again == 'n':
+            played,p1_won,cmp_won=game(played,p1_won,cmp_won)
+            while True:
+                game_menu = int(input(
+                    "Game Menu:\n" +
+                        "1.Show score\n" + 
+                        "2.Reset score\n" +
+                        "3.Play again\n" +
+                        "4.Exit Game\n"
+                ))
+                if game_menu == 1:      #Show score
+                    print("Show score:")
+                    print("GAMES PLAYED : {}".format(played))
+                    print("P1 : {}".format(p1_won))
+                    print("CMP : {}".format(cmp_won))
+                    print("DRAW : {}".format((played -(p1_won + cmp_won))))
+                elif game_menu == 2:    #Reset score
+                    played,p1_won,cmp_won = 0, 0, 0
+                    break
+                elif game_menu == 3:    #Play again
+                    break
+                elif game_menu == 4:    #Exit Game
+                    exit=True
+                    break
+            if exit:
                 break
-    else:
+        save_game = input("Save Game?\n[Y]es\n[N]o\n")[0].lower()
+        if save_game == 'y':
+            output={
+                "name":player_name,
+                "game":{
+                    "played":played,
+                    "won":p1_won,
+                    "lost":cmp_won,
+                    "draw":played-(p1_won+cmp_won)
+                }
+            }
+            print(json.dumps(output))
+            f = open("save_file.json", "w")
+            f.write(json.dumps(output))
+    else:                               #Exit
         print("Exiting game")
